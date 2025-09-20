ccc39a3d22f414ce8fd13f23396af90d6c828fcb
patrick kelava <87270681+pkcode94@users.noreply.github.com>
2025-09-20 08:30:07 +0200

    Projektdateien hinzufügen.

---
+++ Tracker/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+
+using System;
+using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+
+namespace Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class idmanager
+        {
+
+            public static int id;
+            public static int getid()
+            {
+                return id++;
+            }
+        }
+
+
+
+        public Dictionary<string, int> stringtoidmapping = new Dictionary<string, int>();
+        public class pattern
+        {
+            public List<int> patternsequence = new List<int>();
+        }
+        public pattern totalpattern = new pattern();
+        public List<pattern> subpatterns = new List<pattern>();
+        private static readonly HttpClient client = new HttpClient();
+        public void recalculatesubpatterns()
+        {
+             
+        }
+        Dictionary<Tuple<double, double>, int[]> deltacoordinatestopatternidmapping;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string url = "https://gematrix.org";
+
+            // This line blocks the thread until the response is received
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            // Throws an exception on non-success status codes
+            response.EnsureSuccessStatusCode();
+
+            // This line blocks the thread until the content is read
+            string content = response.Content.ReadAsStringAsync().Result;
+            string[] split = content.Split(new string[]{ "<a href=\"/?word=" },StringSplitOptions.RemoveEmptyEntries);
+            List<string> result = new List<string>();
+            for(int i = 10; i < split.Count(); i++)
+            {
+                result.Add(split[i].Split(new string[] { "\">" }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                listBox1.Items.Add(result[result.Count-1]);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+                int newid = idmanager.getid();
+                stringtoidmapping[result[result.Count - 1]] = newid;
+                totalpattern.patternsequence.Add(newid);
+                recalculatesubpatterns();
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 5000;
+
+            MessageBox.Show("TorchSharp Model Definition Example");
+
+            // Setze einen manuellen Seed für Reproduzierbarkeit
+            torch.manual_seed(42);
+
+            // Erstelle eine Modellinstanz
+            var model = new MyComplexModel();
+
+            // Gib eine Zusammenfassung der Modellschichten und Parameter aus
+            MessageBox.Show("\nModellarchitektur:");
+            
+
+
+            // Definiere eine konzeptionelle Eingabedatenform:
+            // (batch_size, sequence_length, embedding_dim)
+            int batchSize = 1;
+            int sequenceLength = 10;
+            int inputFeatures = 256;
+            var inputTensor = torch.randn(batchSize, sequenceLength, inputFeatures);
+
+            // Führe einen Forward-Pass mit den Dummy-Eingabedaten durch
+            MessageBox.Show("\nFühre einen Forward-Pass mit Dummy-Daten durch...");
+            var output = model.forward(inputTensor);
+
+            MessageBox.Show("\nForward-Pass abgeschlossen.");
+            MessageBox.Show($"Shape des Eingabe-Tensors: {inputTensor.shape}");
+            MessageBox.Show($"Shape des Ausgabe-Tensors: {output.shape}");
+
+        }
+    }
+}
---
+++ Tracker/nn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracker
+{
+    internal class nn
+    {
+        //self referential feedback loop
+        //blind feedback loop
+        //transition
+        //input invariance threshold
+        //bruteforce conversation fragments to neural network mapping until pattern is consistent
+        //gradient descent relative to input
+        //bruteforcing combinatoric iomapping gradient X input where the gradient strengthens cumulatively with each successive input 
+        //gradient decay
+        //normalization
+        //learning rate
+        public class IO
+        {
+            public int[] IOVECTOR;
+        }
+        public class inputdimensions
+        {
+            public int maxlength;
+            public int offset = 0;
+        }
+        public class iomapping
+        {
+            public Dictionary<int[], int[]> iomatrix = new Dictionary<int[], int[]>();
+        }
+    }
+}
